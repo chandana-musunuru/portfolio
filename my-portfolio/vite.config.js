ff7ea3fbb6c46098d382d8473398b23edf0c2f7a
chandana-musunuru <musunuruchandana@gmail.com>
2025-04-18 21:35:11 -0700

    Updates

--- my-portfolio/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/chandana portfolio/",  // 👈 add this line
-})
